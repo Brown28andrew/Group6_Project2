6ea415a7d6e64876a5fd4bb5c58e2baf0deb661c
Nifemi26 <nifemi-fadahunsi@ku.edu>
2021-09-09 15:36:11 -0500

    Adding the grid.h to github

--- Grid.h
+++ Grid.h
@@ -1,10 +1,19 @@
 #ifndef GRID_H
 #define GRID_H
 #include "Executive.h"
-
+// The storage of the grid changes
 class Grid {
     public:
+        char** P1_array_grid; 
+        char** P2_array_grid;
+        ~Grid();
+        Grid();
 
     private:
+        const int rows = 9;
+        const int columns = 10;
+        char ships = 'S';
+        char hit_ship = 'H';
+        char miss_ship = 'M';
 };
 #endif
