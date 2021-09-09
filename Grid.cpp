b17e9643b3292fc831d82deb7c7c1d0eb68cc3c2
Yuri Martinez <martinez.moylan@ku.edu>
2021-09-09 18:32:46 -0500

    wrote Grid() and ~Grid() methods. Creates and deletes P1_array_grid with no memory leaks.

--- Grid.cpp
+++ Grid.cpp
@@ -1,2 +1,29 @@
 #include "Grid.h"
 
+/**
+* @pre: none
+* @post: P1_array_grid made with initial value 'O' in each board position.
+*/
+Grid::Grid()
+{
+	P1_array_grid = new char*[rows];
+
+	for(int i = 0; i < rows; i++)
+	{
+		P1_array_grid[i] = new char[columns];
+		
+		for(int j = 0; j < columns; j++)
+			P1_array_grid[i][j] = 'O';
+	}
+}
+/**
+* @pre: none
+* @post: P1_array_grid is deleted
+*/
+Grid::~Grid()
+{
+	for (int i = 0; i < rows; i++)
+		delete[] P1_array_grid[i];
+	
+	delete[] P1_array_grid;
+}
--- Grid.h
+++ Grid.h
@@ -1,16 +1,27 @@
 #ifndef GRID_H
 #define GRID_H
-#include "Executive.h"
+
 // The storage of the grid changes
 class Grid {
     public:
-        char** P1_array_grid; 
-        char** P2_array_grid;
-        ~Grid();
+		/**
+		* @pre: none
+		* @post: P1_array_grid made with initial value 'O' in each board position.
+		*/
         Grid();
+		
+		/**
+		* @pre: none
+		* @post: P1_array_grid is deleted
+		*/
+        ~Grid();
+		
+
 
     private:
-        const int rows = 9;
+        char** P1_array_grid; 
+        //char** P2_array_grid;
+		const int rows = 9;
         const int columns = 10;
         char ships = 'S';
         char hit_ship = 'H';
