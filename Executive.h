803dd5490316000454b4e7d3d570fc0e420785fb
Nifemi26 <nifemi-fadahunsi@ku.edu>
2021-09-12 22:44:27 -0500

    This was to push the changes to Executive.h

--- Executive.h
+++ Executive.h
@@ -3,8 +3,18 @@
 
 class Executive {
     public:
+    Executive();
+    ~Executive();
+    void BeginGame();
 
     private:
+    char col;
+    int originRow;
+    char direction;
+    int shipsize;
+    int originCol = 0;
+    char colLetter = A;
+
 
 };
 #endif
