6ded49903336d2971e45258f16a8d8e28f9dd816
Yuri Martinez <martinez.moylan@ku.edu>
2021-09-13 22:07:09 -0500

    Defined isHit, getEndGame, and setHitsLeft methods for the Game class.

--- Game.cpp
+++ Game.cpp
@@ -1,3 +1,26 @@
+#include "Game.h"
 #include "Grid.h"
 
 
+bool Game::isHit(int tRow, int tCol, Grid tGrid)
+{
+	if (tGrid.getValue(tRow, tCol) == '|' || tGrid.getValue(tRow, tCol) == '-')
+	{
+		
+	}
+	return false;
+}
+
+bool Game::getEndGame()
+{
+	if (hitsLeft == 0)
+		return true;
+	
+	else
+		return false;
+}
+
+void Game::setHitsLeft(int shipNum)
+{
+	hitsLeft = (shipNum * (shipNum + 1))/2;
+}
--- Game.h
+++ Game.h
@@ -1,10 +1,32 @@
-#ifndef OUTPUT_H
-#define OUTPUT_H
-#include "Executive.h"
+#ifndef GAME_H
+#define GAME_H
+#include "Grid.h"
 // This is what controls grid and gives it the commands 
-class Output {
-    public: 
-
+class Game {
+    public:
+	
+	/**
+	* @brief: calls for getValue() at grid[tRow][tCol], verifies if it's a hit or a miss,
+	*         updates value at that location, and updates hitsLeft and endGame if needed.
+	* @return: true if hit, false if miss
+	*/
+	bool isHit(int tRow, int tCol, Grid tGrid);
+	
+	/**
+	* @return: true if hitsLeft = 0, false otherwise
+	*/
+	bool getEndGame();
+	
+	/**
+	* @brief: assigns (n(n+1))/2 to hitsLeft, where n = number of ships per player.
+	*/
+	void setHitsLeft(int shipNum);
+	
+	
+	
     private:
+	
+	//number that decrements with each hit, down to 0.
+	int hitsLeft;
 };
 #endif
