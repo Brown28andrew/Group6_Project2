01d99c1c07cad6001eeb10f2d57c62c139733d71
Nifemi26 <nifemi-fadahunsi@ku.edu>
2021-09-15 20:24:07 -0500

    Edited executive for player 1 and 2 ships collection

--- Executive.cpp
+++ Executive.cpp
@@ -8,11 +8,13 @@ using namespace std;
 Executive::Executive()
 {
 	player1 = new Grid;
+    player2 = new Grid;
 }
 
 Executive::~Executive()
 {
 	delete player1;
+    delete player2;
 }
 
 void Executive::BeginGame()
@@ -25,17 +27,33 @@ void Executive::BeginGame()
     cout << "The symbol for ships: | or -\n";
     cout << "The symbol for when ships are hit: H\n";
     cout << "The symbol for when shots are missed: M\n";
-    cout << "How many ships would you like to have in the game?\nNumber of ships (1-6): ";
+    cout << "How many ships would you like to have in the game Player 1?\nNumber of ships (1-6): ";
     cin >> numShips;
         while (numShips > 6 || numShips < 0)
         {
             cout << "Invalid number. How many ships do you want to place? (1-6) \n";
             cin >> numShips;
         }
-        
+    char readiness;     
     player1->print_ships_Grid();
     placeShips(numShips, player1);
+    cout << " Thanks Player1!\n";
+    cout << "Player 2, Are you ready?\n";
+    cin >> readiness; 
 
+    for (int i =0; i< 50; i++)
+    {
+        cout <<"\n";
+    }
+
+    if (readiness == 'N' || readiness == 'n')
+    {
+        cout << "Too bad, it's already started...";
+    }
+    cout << "Player 2, its your turn to place your " << numShips << " ships!\n";
+        
+    player2->print_ships_Grid();
+    placeShips(numShips, player2);
 }
 
 void Executive::placeShips(int numShips, Grid* playerGrid) 
@@ -52,6 +70,7 @@ void Executive::placeShips(int numShips, Grid* playerGrid)
         
         cout << "Where would you like to place the origin of ship "<< i+1 << "?\nColumn (A-J): ";
         cin >> col;
+        col = toupper(col);
             while (colLetter < col)//loop to turn letter for column into integer to place ship on grid
             {
                 colLetter++;
@@ -88,9 +107,9 @@ void Executive::playGame()
         {
             
             player1->print_shots_Grid(); //print player ones shot grid
-            player1->print_shots_Grid();//print player ones ship grid
+            player1->print_ships_Grid();//print player ones ship grid
 
-            cout << "Where would you like to take your shot?\nColumn (A-J):"; //get shot from player one
+            cout << "Where would you like to take your shot Player 1?\nColumn (A-J):"; //get shot from player one
             char shotColumn;
             cin >> shotColumn;
             cout << "Row (1-9): ";
@@ -104,9 +123,15 @@ void Executive::playGame()
         }
         else
         {
-            //print player twos shot grid
-            //print player twos ship grid
-            //get shot from player two
+            /*player2->print_shots_Grid(); //print player ones shot grid
+            player2->print_ships_Grid();//print player ones ship grid
+
+            cout << "Where would you like to take your shot Player 2 ?\nColumn (A-J):"; //get shot from player one
+            char shotColumn;
+            cin >> shotColumn;
+            cout << "Row (1-9): ";
+            int shotRow;
+            cin >> shotRow;*/ 
             //check if hit or miss
             //update board
             //check if game end
