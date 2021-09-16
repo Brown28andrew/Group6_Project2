02d66e21c386cd35bf5c1aacbecf470ac5444a45
Yuri Martinez <martinez.moylan@ku.edu>
2021-09-15 20:59:24 -0500

    removed function calls in main.cpp that weren't useful anymore.

--- main.cpp
+++ main.cpp
@@ -7,17 +7,8 @@ using namespace std;
 
 int main()
 {
-    //gameIntro();
-    //func_legend();
-    //grid_make();
-    //user_pos_grid();
-    //checkForValidPlacedCoordinates();
-    //printUserGrid();
     Executive myExec;
     myExec.BeginGame();
-    //Grid myGrid;
-    //myGrid.setShip(5,5,'U',1);
-    //myGrid.printUserGrid();
 
     return (0);
 }
